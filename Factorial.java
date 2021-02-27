ac8f4f7c49727c3ae78b15f2540a0d5cc6c00db3
Aishwarya Kadam <79749819+aishukadam@users.noreply.github.com>
2021-02-27 17:46:42 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+class Factorial
+{
+   public static void main(String[]args)
+   {
+    int i,fact=1,no=7;
+    for(i=1;i<no;i++)
+     {
+       fact=fact*i;
+     } 
+    System.out.println("factorial of no 7 is:" +fact);
+
+   }
+
+}
